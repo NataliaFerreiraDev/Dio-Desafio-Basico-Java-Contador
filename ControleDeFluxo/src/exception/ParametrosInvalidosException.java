a43c1f28284ca240122217fe8be220a02eb55929
Natalia Ferreira <nataliaferreira.dev@yahoo.com.br>
2023-02-16 15:50:13 -0300

    Desafio Controle de Fluxo

---
+++ ControleDeFluxo/src/exception/ParametrosInvalidosException.java
@@ -0,0 +1,9 @@
+package exception;
+
+public class ParametrosInvalidosException extends Exception {
+
+    public ParametrosInvalidosException(){
+        super("O segundo parâmetro deve ser maior que o primeiro!");
+    }
+
+}
